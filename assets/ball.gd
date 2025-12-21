5c31bc7aba6ce78f71ec859a1679c43f48ae09f3
splinecraft <splinecraft.anim@gmail.com>
2025-12-20 17:37:11 -0800

    paddle deflection

--- assets/ball.gd
+++ assets/ball.gd
@@ -1,19 +1,25 @@
 extends CharacterBody2D
 
-@export var speed := 200.0
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	velocity = Vector2(100, 100)
+	velocity = Vector2(100, 250)
 	
 func _physics_process(delta: float) -> void:
-	position += velocity * delta
-	move_and_slide()
+	var collision = move_and_collide(velocity * delta)
 	
-	if get_last_slide_collision():
-		var collider = get_last_slide_collision().get_collider()
-		if collider.is_in_group("Player") or collider.is_in_group("Ceiling"):			
+	if collision:
+		var collider = collision.get_collider()
+		if collider.is_in_group("Player"):			
+			var paddle_center = collider.global_position.x
+			var ball_pos = collision.get_position().x
+			var distance_from_center = ball_pos - paddle_center
+			
+			var speed = velocity.length()
+			velocity.y = -abs(velocity.y)
+			velocity.x += distance_from_center * 5
+			velocity = velocity.normalized() * speed
+		elif collider.is_in_group("Ceiling"):
 			velocity.y = -velocity.y
 		elif collider.is_in_group("Wall"):		
 			velocity.x = -velocity.x
